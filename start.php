77fcffdb312cd92eeaf8e8aa707a9be6f398d234
Maxime Dizerens <mdizerens@gmail.com>
2012-04-13 02:12:40 +0300

    forgot 2 slashes...

--- start.php
+++ start.php
@@ -33,11 +33,11 @@ class Purifier {
                 {
                     // Load the all of HTML Purifier right now.
                     // This increases performance with a slight hit to memory usage.
-                    require dirname(__FILE__) . 'library/HTMLPurifier.includes.php';
+                    require dirname(__FILE__) . '/library/HTMLPurifier.includes.php';
                 }
 
                 // Load the HTML Purifier auto loader
-                require dirname(__FILE__) . 'library/HTMLPurifier.auto.php';
+                require dirname(__FILE__) . '/library/HTMLPurifier.auto.php';
             }
 
             // Create a new configuration object
