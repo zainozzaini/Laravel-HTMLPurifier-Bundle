e10649aa4c6188291fdc108b32929486847902ff
Maxime Dizerens <mdizerens@gmail.com>
2013-02-24 18:05:21 +0100

    Update HTMLPurifier to 4.5.0 and switch to IoC container provided by Laravel

--- config/purifier.php
+++ config/purifier.php
@@ -2,7 +2,7 @@
 
 /*
  * This file is part of HTMLPurifier Bundle.
- * (c) 2012 Maxime Dizerens
+ * (c) 2012-2013 Maxime Dizerens
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- start.php
+++ start.php
@@ -2,114 +2,48 @@
 
 /*
  * This file is part of HTMLPurifier Bundle.
- * (c) 2012 Maxime Dizerens
+ * (c) 2012-2013 Maxime Dizerens
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
-class Purifier {
-
-    /**
-     * @var  HTMLPurifier  singleton instance of the HTML Purifier object
-     */
-    protected static $singleton;
-
-    /**
-     * Returns the singleton instance of HTML Purifier. If no instance has
-     * been created, a new instance will be created.
-     *
-     *     $purifier = Purifier::instance();
-     *
-     * @return  HTMLPurifier
-     */
-    public static function instance()
+Laravel\IoC::singleton('HTMLPurifier', function()
+{
+    if ( ! class_exists('HTMLPurifier_Config', false))
     {
-        if ( ! Purifier::$singleton)
+        if (Config::get('purifier.preload'))
         {
-            if ( ! class_exists('HTMLPurifier_Config', false))
-            {
-                if (Config::get('purifier.preload'))
-                {
-                    // Load the all of HTML Purifier right now.
-                    // This increases performance with a slight hit to memory usage.
-                    require dirname(__FILE__) . '/library/HTMLPurifier.includes.php';
-                }
-
-                // Load the HTML Purifier auto loader
-                require dirname(__FILE__) . '/library/HTMLPurifier.auto.php';
-            }
-
-            // Create a new configuration object
-            $config = HTMLPurifier_Config::createDefault();
-
-            if ( ! Config::get('purifier.finalize'))
-            {
-                // Allow configuration to be modified
-                $config->autoFinalize = false;
-            }
-
-            // Use the same character set as Laravel
-            $config->set('Core.Encoding', Config::get('application.encoding'));
-
-            if (is_array($settings = Config::get('purifier.settings')))
-            {
-                // Load the settings
-                $config->loadArray($settings);
-            }
-
-            // Configure additional options
-            $config = Purifier::configure($config);
-
-            // Create the purifier instance
-            Purifier::$singleton = new HTMLPurifier($config);
+            // Load the all of HTML Purifier right now.
+            // This increases performance with a slight hit to memory usage.
+            require dirname(__FILE__) . '/library/HTMLPurifier.includes.php';
         }
-
-        return Purifier::$singleton;
+        
+        // Load the HTML Purifier auto loader
+        require dirname(__FILE__) . '/library/HTMLPurifier.auto.php';
     }
-
-    /**
-     * Modifies the configuration before creating a HTML Purifier instance.
-     *
-     * [!!] You must create an extension and overload this method to use it.
-     *
-     * @param   HTMLPurifier_Config  configuration object
-     * @return  HTMLPurifier_Config
-     */
-    public static function configure(HTMLPurifier_Config $config)
+    
+    // Create a new configuration object
+    $config = HTMLPurifier_Config::createDefault();
+    
+    if ( ! Config::get('purifier.finalize'))
     {
-        return $config;
+        // Allow configuration to be modified
+        $config->autoFinalize = false;
     }
-
-    /**
-     * Removes broken HTML and XSS from text using [HTMLPurifier](http://htmlpurifier.org/).
-     *
-     *     $text = Purifier::clean($dirty_html);
-     *
-     * The original content is returned with all broken HTML and XSS removed.
-     *
-     * @param   mixed   text to clean, or an array to clean recursively
-     * @return  mixed
-     */
-    public static function clean($dirty)
+    
+    // Use the same character set as Laravel
+    $config->set('Core.Encoding', Config::get('application.encoding'));
+    
+    if (is_array($settings = Config::get('purifier.settings')))
     {
-        if (is_array($dirty))
-        {
-            foreach ($dirty as $key => $value)
-            {
-                // Recursively clean arrays
-                $clean[$key] = Purifier::clean($value);
-            }
-        }
-        else
-        {
-            // Load HTML Purifier
-            $purifier = Purifier::instance();
-
-            // Clean the HTML and return it
-            $clean = $purifier->purify($dirty);
-        }
-
-        return $clean;
+        // Load the settings
+        $config->loadArray($settings);
     }
-}
+    
+    // Configure additional options
+    $config = HTMLPurifier_Config::create($config);
+    
+    // Return the purifier instance
+    return new HTMLPurifier($config);
+});
